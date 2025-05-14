8a5abb334cf6d17b3929e9c1bcde8dfb0d031d11
Kilyong Jeong <uzoolove@gmail.com>
2025-05-14 23:17:35 +0900

    🧪

--- src/pages/naverapi/main.ts
+++ src/pages/naverapi/main.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const clientId = 'xxxxxxxxx';
-const clientSecret = 'xxxxxxxxx';
+const clientId = 'nurYihykXbeXcAVh1eYD';
+const clientSecret = 'spjqOUyM9q';
 
 async function naverNews() {
   const URL = `https://openapi.naver.com/v1/search/news.json?query=hello`;
