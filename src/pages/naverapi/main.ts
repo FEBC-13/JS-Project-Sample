6f40b486acf56f9ee6fc842af885f10690a9f12c
Kilyong Jeong <uzoolove@gmail.com>
2025-05-15 14:28:41 +0900

    ✨

--- src/pages/naverapi/main.ts
+++ src/pages/naverapi/main.ts
@@ -5,12 +5,14 @@ const clientSecret = 'spjqOUyM9q';
 
 async function naverNews() {
   // const URL = `https://openapi.naver.com/v1/search/news.json?query=hello`;
-  const URL = `/naver/v1/search/news.json?query=hello`;
+  // const URL = `/naver/v1/search/news.json?query=hello`;
+  const URL = `https://fesp-api.koyeb.app/proxy/v1/search/news.json?query=hello`;
 
   axios.get(URL, {
     headers: {
       'X-Naver-Client-Id': clientId,
       'X-Naver-Client-Secret': clientSecret,
+      'X-Target-Url': 'https://openapi.naver.com', // 생략시 기본값 'https://openapi.naver.com'
     },
   });
 }
